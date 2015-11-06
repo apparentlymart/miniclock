516b26682efe5b990f3d9e8dbc1b4999ca119be1
Martin Atkins <mart@degeneration.co.uk>
2015-11-05 23:54:49 -0800

    Sleep and use the RTC's 1Hz square wave to wake up every 0.5s.
    Still just more prototyping, in this case making sure that we can use the
    1Hz SQW pin as an interrupt and sleep between them.

--- firmware/main.c
+++ firmware/main.c
@@ -19,14 +19,14 @@ void main() {
     SYSCON_PINTSEL1 = 13; // Down
     SYSCON_PINTSEL2 = 3; // Left
     SYSCON_PINTSEL3 = 2; // Right
-    SYSCON_PINTSEL4 = 10; // 1Hz square wave from RTC
+    SYSCON_PINTSEL4 = 16; // 1Hz square wave from RTC
 
     // Enable IRQs for the GPIO interrupts.
     NVIC_ISER0 |= BIT24;
     NVIC_ISER0 |= BIT25;
     NVIC_ISER0 |= BIT26;
     NVIC_ISER0 |= BIT27;
-    //NVIC_ISER0 |= BIT28;
+    NVIC_ISER0 |= BIT28;
 
     // Turn on detection of both rising and falling edges.
     PINT_SIENR = 0b11111;
@@ -42,7 +42,15 @@ void main() {
     uart_init();
     i2c_init();
     uart_puts("\x1b[2J");
+    // Set control register 0xe to enable the 1Hz SQW signal.
+    i2c_puts("\x0e\x00", 2);
     while (1) {
+
+        // Wait until we get an interrupt.
+        // Since we have a 1Hz square wave coming in on
+        // GPIO16, we'll wake up at least twice per second.
+        __asm volatile ("wfi");
+
         if (pressed) {
             uart_println("Button press world!\r\n");
         }
@@ -50,7 +58,7 @@ void main() {
             i2c_puts("", 1); // write null for address 0
             char buf[3];
             i2c_gets((char*)&buf, 3);
-            int time_val = ((buf[2] & 0b11111) << 16) | (buf[1] << 8) | buf[0];
+            int time_val = ((buf[2] & 0b11111) << 16) | (buf[1] << 8) | buf[0] | (GPIO_W16 << 24);
             uart_puts("\x1b[0;0H");
             uart_put_int_hex(time_val);
         }
